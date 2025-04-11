d151109f78cd813acb558f542af6313417e02f87
ashortsleeves <ashortsleeves@gmail.com>
2025-04-11 11:40:57 -0400

    basic frontend styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+import folder from './icons/folder.svg';
+import fork from './icons/fork.svg';
+import letter from './icons/letter.svg';
+import linkedin from './icons/linkedin.svg';
+import github from './icons/github.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hi! I'm Adam.</h1>
+        <p>I build websites.</p>
+        <ul class="as-links">
+          <li><a href="#">My Work <img src={folder} className='as-icon as-icon__folder' alt="folder Icon" /></a></li>
+          <li><a href="#">Side Projects <img src={fork} className='as-icon as-icon__fork' alt="fork Icon" /></a></li>
+          <li><a href="#">Contact Me <img src={letter} className='as-icon as-icon__contact' alt="letter Icon" /></a></li>
+          <li><a href="https://www.linkedin.com/in/adam-shortsleeves/">LinkedIn <img src={linkedin} className='as-icon as-icon__linkedin' alt="linkedin Icon" /></a></li>
+          <li><a href="https://github.com/ashortsleeves">GitHub <img src={github} className='as-icon as-icon__github' alt="github Icon" /></a></li>
+        </ul>
       </header>
     </div>
   );
