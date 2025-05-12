a076842b073c18730961d5e9233f545dafb48fa4
ashortsleeves <ashortsleeves@gmail.com>
2025-05-12 15:42:15 -0400

    styling fixes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,6 +5,9 @@ export default function Work() {
 
     return (
         <div className="as-work">
+            <div className="as-description">
+                <p>I do freelance website design and development. <span>I also work for other agencies and provide ongoing support for their clients.</span></p>
+            </div>
             {sites.map((site, index) => (
                 <a href={site.url} key={index} target='_blank' rel="noreferrer">
                     <p>
